69feadecf685f6c7b72381677dfded86125bf197
Marcello Cordeiro <maarc.hen@gmail.com>
2018-06-14 13:32:25 -0400

    updating code

--- src/controller/itemController.js
+++ src/controller/itemController.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
 
-exports.get = (req, res) => {
-    let itensJson = fs.readFileSync('modelo-resposta-api.json');
-    let itens = JSON.parse(itensJson);
+let itensJson = fs.readFileSync('modelo-resposta-api.json');
+let itens = JSON.parse(itensJson);
 
+exports.get = (req, res) => {
     res.send(itens);
 };
 
 exports.getRecommendation = (req, res) => {
-    let itensJson = fs.readFileSync('modelo-resposta-api.json');
-    let itens = JSON.parse(itensJson);
-
     let filterItem = itens.data.recommendation.filter(elem => elem.businessId === req.params.id);
     res.send(filterItem);
 }
